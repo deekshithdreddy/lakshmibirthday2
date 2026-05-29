5d24b34a354b928e42f773bca0c58e46d481fec4
DONTIREDDY LAKSHMAN DEEKSHITH REDDY <165643636+deekshithdreddy@users.noreply.github.com>
2026-05-29 10:59:21 +0530

    hero edit

--- src/routes/home.tsx
+++ src/routes/home.tsx
@@ -63,7 +63,7 @@ function Navbar() {
 }
 
 function Hero() {
-  const heroImg = '/images/lakshmi-beach.jpg'; 
+  const heroImg = '/images/lakshmilaugh.jpg'; 
   return (
     <section
       className="hero saga-hero"
